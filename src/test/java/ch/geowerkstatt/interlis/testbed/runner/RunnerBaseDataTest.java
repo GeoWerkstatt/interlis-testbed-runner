031b415749fd9ab62f169edfb6b218b10e1219e6
Dominic Burger <dominic.burger@geowerkstatt.ch>
2023-12-12 14:13:54 +0100

    Split runner tests by test data directory

--- src/test/java/ch/geowerkstatt/interlis/testbed/runner/RunnerBaseDataTest.java
+++ src/test/java/ch/geowerkstatt/interlis/testbed/runner/RunnerBaseDataTest.java
@@ -20,11 +20,11 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public final class RunnerTest extends MockitoTestBase {
+public final class RunnerBaseDataTest extends MockitoTestBase {
     private static final Path BASE_PATH = Path.of("src/test/data/testbed").toAbsolutePath().normalize();
-    private static final Path BASE_PATH_WITH_PATCHES = Path.of("src/test/data/testbed-with-patches").toAbsolutePath().normalize();
-    private static final Path ILI_VALIDATOR_PATH = Path.of("ilivalidator.jar");
+    private static final Path BASE_DATA_FILE = BASE_PATH.resolve("data.xtf");
 
+    private TestOptions options;
     private TestLogAppender appender;
     @Mock
     private Validator validatorMock;
@@ -33,6 +33,7 @@ public final class RunnerTest extends MockitoTestBase {
 
     @BeforeEach
     public void setup() {
+        options = new TestOptions(BASE_PATH, Path.of("ilivalidator.jar"));
         appender = TestLogAppender.registerAppender(Runner.class);
     }
 
@@ -46,7 +47,6 @@ public final class RunnerTest extends MockitoTestBase {
     public void runValidatesBaseData() throws IOException, ValidatorException {
         when(validatorMock.validate(any(), any())).thenReturn(true);
 
-        var options = new TestOptions(BASE_PATH, ILI_VALIDATOR_PATH);
         var runner = new Runner(options, validatorMock, mergerMock);
 
         var runResult = runner.run();
@@ -56,19 +56,17 @@ public final class RunnerTest extends MockitoTestBase {
         var errors = appender.getErrorMessages();
         assertIterableEquals(List.of("No patch files found."), errors);
 
-        var expectedBaseDataFile = BASE_PATH.resolve("data.xtf");
         var baseDataFile = options.baseDataFilePath();
         assertFalse(baseDataFile.isEmpty(), "Base data file should have been found.");
-        assertEquals(expectedBaseDataFile, baseDataFile.get());
+        assertEquals(BASE_DATA_FILE, baseDataFile.get());
 
-        verify(validatorMock).validate(eq(expectedBaseDataFile), any());
+        verify(validatorMock).validate(eq(BASE_DATA_FILE), any());
     }
 
     @Test
     public void runLogsValidationError() throws ValidatorException {
         when(validatorMock.validate(any(), any())).thenReturn(false);
 
-        var options = new TestOptions(BASE_PATH, ILI_VALIDATOR_PATH);
         var runner = new Runner(options, validatorMock, mergerMock);
 
         var runResult = runner.run();
@@ -82,33 +80,7 @@ public final class RunnerTest extends MockitoTestBase {
                 Pattern.matches("Validation of .*? failed\\..*", errorMessage),
                 "Error message should start with 'Validation of <base data file> failed.', actual value: '" + errorMessage + "'."
         );
-    }
 
-    @Test
-    public void runMergesAndValidatesPatchFiles() throws ValidatorException {
-        var expectedBaseDataFile = BASE_PATH_WITH_PATCHES.resolve("data.xtf");
-        var expectedPatchFile = BASE_PATH_WITH_PATCHES.resolve("constraintA").resolve("testcase-1.xtf");
-        var expectedMergedFile = BASE_PATH_WITH_PATCHES.resolve("output").resolve("constraintA").resolve("testcase-1_merged.xtf");
-
-        when(validatorMock.validate(eq(expectedBaseDataFile), any())).thenReturn(true);
-        when(validatorMock.validate(eq(expectedMergedFile), any())).thenReturn(false);
-
-        when(mergerMock.merge(any(), any(), any())).thenReturn(true);
-
-        var options = new TestOptions(BASE_PATH_WITH_PATCHES, ILI_VALIDATOR_PATH);
-
-        var runner = new Runner(options, validatorMock, mergerMock);
-
-        var runResult = runner.run();
-
-        assertTrue(runResult, "Testbed run should have succeeded.");
-
-        var errors = appender.getErrorMessages();
-        assertTrue(errors.isEmpty(), "No errors should have been logged.");
-
-        verify(validatorMock).validate(eq(expectedBaseDataFile), any());
-        verify(validatorMock).validate(eq(expectedMergedFile), any());
-
-        verify(mergerMock).merge(eq(expectedBaseDataFile), eq(expectedPatchFile), eq(expectedMergedFile));
+        verify(validatorMock).validate(eq(BASE_DATA_FILE), any());
     }
 }
---
+++ src/test/java/ch/geowerkstatt/interlis/testbed/runner/RunnerWithConstraintsTest.java
@@ -0,0 +1,110 @@
+package ch.geowerkstatt.interlis.testbed.runner;
+
+import ch.geowerkstatt.interlis.testbed.runner.xtf.XtfMerger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.nio.file.Path;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public final class RunnerWithConstraintsTest extends MockitoTestBase {
+    private static final Path BASE_PATH = Path.of("src/test/data/testbed-with-patches").toAbsolutePath().normalize();
+    private static final String CONSTRAINT_NAME = "constraintA";
+    private static final Path BASE_DATA_FILE = BASE_PATH.resolve("data.xtf");
+    private static final Path PATCH_FILE = BASE_PATH.resolve(CONSTRAINT_NAME).resolve("testcase-1.xtf");
+    private static final Path MERGED_FILE = BASE_PATH.resolve("output").resolve(CONSTRAINT_NAME).resolve("testcase-1_merged.xtf");
+    private static final Path MERGED_LOG_FILE = BASE_PATH.resolve("output").resolve(CONSTRAINT_NAME).resolve("testcase-1.log");
+
+    private TestOptions options;
+    private TestLogAppender appender;
+    @Mock
+    private Validator validatorMock;
+    @Mock
+    private XtfMerger mergerMock;
+
+    @BeforeEach
+    public void setup() {
+        options = new TestOptions(BASE_PATH, Path.of("ilivalidator.jar"));
+        appender = TestLogAppender.registerAppender(Runner.class);
+
+        when(mergerMock.merge(any(), any(), any())).thenReturn(true);
+    }
+
+    @AfterEach
+    public void teardown() {
+        appender.stop();
+        appender.unregister();
+    }
+
+    @Test
+    public void runMergesAndValidatesPatchFiles() throws ValidatorException {
+        when(validatorMock.validate(eq(BASE_DATA_FILE), any())).thenReturn(true);
+        when(validatorMock.validate(MERGED_FILE, MERGED_LOG_FILE)).thenReturn(false);
+
+        var runner = new Runner(options, validatorMock, mergerMock);
+
+        var runResult = runner.run();
+
+        assertTrue(runResult, "Testbed run should have succeeded.");
+
+        var errors = appender.getErrorMessages();
+        assertTrue(errors.isEmpty(), "No errors should have been logged.");
+
+        verify(validatorMock).validate(eq(BASE_DATA_FILE), any());
+        verify(validatorMock).validate(eq(MERGED_FILE), eq(MERGED_LOG_FILE));
+
+        verify(mergerMock).merge(eq(BASE_DATA_FILE), eq(PATCH_FILE), eq(MERGED_FILE));
+    }
+
+    @Test
+    public void runFailsIfMergeFails() throws ValidatorException {
+        when(validatorMock.validate(eq(BASE_DATA_FILE), any())).thenReturn(true);
+        when(mergerMock.merge(eq(BASE_DATA_FILE), eq(PATCH_FILE), any())).thenReturn(false);
+
+        var runner = new Runner(options, validatorMock, mergerMock);
+
+        var runResult = runner.run();
+
+        assertFalse(runResult, "Testbed run should have failed if file merging failed.");
+
+        verify(validatorMock).validate(eq(BASE_DATA_FILE), any());
+        verify(validatorMock, never()).validate(eq(MERGED_FILE), eq(MERGED_LOG_FILE));
+
+        verify(mergerMock).merge(eq(BASE_DATA_FILE), eq(PATCH_FILE), eq(MERGED_FILE));
+    }
+
+    @Test
+    public void runFailsIfMergedFileIsValid() throws ValidatorException {
+        when(validatorMock.validate(eq(BASE_DATA_FILE), any())).thenReturn(true);
+        when(validatorMock.validate(MERGED_FILE, MERGED_LOG_FILE)).thenReturn(true);
+
+        var runner = new Runner(options, validatorMock, mergerMock);
+
+        var runResult = runner.run();
+
+        assertFalse(runResult, "Testbed run should have failed if merged file is valid.");
+
+        var errors = appender.getErrorMessages();
+        assertEquals(1, errors.size(), "One error should have been logged.");
+        var errorMessage = errors.getFirst();
+        assertTrue(
+                Pattern.matches("Validation of .*? was expected to fail but completed successfully\\.", errorMessage),
+                "Expected: Validation of <merged xtf file> was expected to fail but completed successfully. Actual: '" + errorMessage + "'.");
+
+        verify(validatorMock).validate(eq(BASE_DATA_FILE), any());
+        verify(validatorMock).validate(eq(MERGED_FILE), any());
+
+        verify(mergerMock).merge(eq(BASE_DATA_FILE), eq(PATCH_FILE), eq(MERGED_FILE));
+    }
+}
